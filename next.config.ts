f352f3b4b243eeee4963591eac499fd21a18aaa7
Deepakeon <eonzolt@gmail.com>
2025-10-24 15:59:37 +0530

    chore: added gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/haunted-halloween-landing',
+  assetPrefix: '/haunted-halloween-landing/'
 };
 
 export default nextConfig;
